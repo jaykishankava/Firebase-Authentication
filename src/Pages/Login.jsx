377395404bb3090c7e879aee48ebf8db70d96788
jaykishankava <kava.jaykishan9125@gmail.com>
2024-10-03 04:39:55 +0530

    task

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import  './Login.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { RiLoginCircleFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
